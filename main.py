0772c883eb08777478202030ddc2db7f5a9dfa21
Takayuki Nishikawa <tt8a02km21113@gmail.com>
2024-07-05 18:01:49 +0900

    [add] first_commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import os
+import tensorflow as tf
+from flask import Flask, request, redirect, render_template, flash
+from werkzeug.utils import secure_filename
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import numpy as np
+
+print(tf.__version__)  # 2.1.0
+
+classes = ["かぼちゃ", "ピーマン", "トマト"]
+image_size = 256
+
+UPLOAD_FOLDER = "uploads"
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'supersecretkey'
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+model = load_model('./my_model_20240705_074144.h5', compile=False)
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            flash('ファイルがありません')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('ファイルがありません')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+            # 受け取った画像を読み込み、np形式に変換
+            img = image.load_img(filepath, color_mode="rgb", target_size=(image_size, image_size))
+            img = image.img_to_array(img)
+            img = np.expand_dims(img, axis=0)
+            img = img / 255.0  # 正規化
+
+            # 変換したデータをモデルに渡して予測する
+            result = model.predict(img)[0]
+            predicted = result.argmax()
+            pred_answer = "これは " + classes[predicted] + " です"
+
+            return render_template("index.html", answer=pred_answer)
+
+    return render_template("index.html", answer="")
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host='0.0.0.0', port=port)
